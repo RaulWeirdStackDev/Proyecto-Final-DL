625222f4367a47c5f37b1824ab32f3d7be039414
EnrikeSekai <eaoo@gmail.com>
2024-12-27 14:57:48 -0300

    Limpiar archivos rastreados previamente y aplicar .gitignore

--- docs/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/Pizzeriamammamia/', // Nombre exacto del repositorio
-  build: {
-    outDir: 'dist', // Carpeta donde se generará la compilación
-  },
-});

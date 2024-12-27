127775628f6848df1f61df5295a0731ef8bae46d
EnrikeSekai <eaoo@gmail.com>
2024-12-27 14:59:27 -0300

    Restaurado

---
+++ frontend/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Pizzeriamammamia/', // Nombre exacto del repositorio
+  build: {
+    outDir: 'dist', // Carpeta donde se generará la compilación
+  },
+});
